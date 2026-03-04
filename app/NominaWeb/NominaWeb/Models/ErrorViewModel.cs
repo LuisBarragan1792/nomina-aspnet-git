f85c5e4323a06d4cdf0b44b96f5d8f1500610930
Luis Barragan <lbarraganv@live.com>
2026-03-03 19:28:35 -0500

    initial commit project asp.net mvc nomina

---
+++ app/NominaWeb/NominaWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NominaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
