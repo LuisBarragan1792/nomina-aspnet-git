82efeb8c37fc3bcad9e0d12eb71f2ecd92641135
Luis Barragan <lbarraganv@live.com>
2026-03-04 21:29:44 -0500

    ui: rename Home to Dashboard

--- app/NominaWeb/NominaWeb/Controllers/HomeController.cs
+++ app/NominaWeb/NominaWeb/Controllers/HomeController.cs
@@ -1,14 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using NominaWeb.Models;
+using NominaWeb.Models.ViewModels;
+using NominaWeb.Data;
 using System.Diagnostics;
 
 namespace NominaWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly NominaDbContext _context;
+
+        public HomeController(NominaDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var dashboard = new DashboardViewModel
+            {
+                TotalEmpleados = _context.Employees.Count(),
+                TotalDepartamentos = _context.Departments.Count(),
+                SalariosVigentes = _context.Salaries.Count(s => s.ToDate == DateTime.MaxValue),
+                SalariosPorVencer = _context.Salaries.Count(s => s.ToDate < DateTime.Now.AddDays(30))
+            };
+
+            return View(dashboard);
         }
 
         public IActionResult Privacy()
@@ -19,7 +36,10 @@ namespace NominaWeb.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
         }
     }
 }
